2e9ef0b65896518400d84d6e52edd28afb37e054
Dheeraj0650 <41461773+Dheeraj0650@users.noreply.github.com>
2023-11-05 02:41:45 -0700

    added information hub project

--- src/Components/Projectspage.jsx
+++ src/Components/Projectspage.jsx
@@ -8,11 +8,11 @@ function Projectspage(){
                <div className="projects-left">
                     <div className="projects-details">
                         <div style={{fontSize:"2rem"}}>Projects - 1</div>
-                        <img src="lambda.png"></img>
+                        <img src="information-hub.png"></img>
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
@@ -23,7 +23,7 @@ function Projectspage(){
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
@@ -34,7 +34,7 @@ function Projectspage(){
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
@@ -45,7 +45,7 @@ function Projectspage(){
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
@@ -56,7 +56,7 @@ function Projectspage(){
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
@@ -67,7 +67,7 @@ function Projectspage(){
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
@@ -78,7 +78,7 @@ function Projectspage(){
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
@@ -89,7 +89,7 @@ function Projectspage(){
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
@@ -100,7 +100,7 @@ function Projectspage(){
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
@@ -111,7 +111,7 @@ function Projectspage(){
                         <div className="info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
                     </div>
                     <div className="projects-view">
-                        <button type="button" class="btn btn-primary">View Details</button>
+                        <button type="button" class="btn btn-primary">Live</button>
                         <button type="button" class="btn btn-outline-dark"><i class="fa-brands fa-github"></i> github</button>
                     </div>
                </div>
